80a7caa39012e9c229a832b707bda6c28429072f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-23 09:46:53 +0000

    [header_manager] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@15966 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = 'Προσθέστε ένα banner';
+$lang['Banner added'] = 'Banner προστέθηκε';
+$lang['Banner deleted'] = 'Banner διαγράφτηκε';
+$lang['Banner'] = 'Banner';
+$lang['Choose an image from your computer'] = 'Επιλέξτε μια εικόνα από τον υπολογιστή σας';
+$lang['Choose the part of the image you want to use as your header.'] = 'Επιλέξτε ένα μέρος της εικόνας που θέλετε να χρησιμοποιήσετε ως header σας';
+$lang['Crop banner image'] = 'Περικοπή εικόνας banner';
+$lang['Default banner size'] = 'Προκαθορισμένο μέγεθος banner ';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Για MontBlancXL και BlancMontXL, προτεινόμενο μέγεθος είναι 900&times;190.';
+$lang['Gallery title above image'] = 'Τίτλος γκαλερί πάνω από την εικόνα';
+$lang['Image only'] = 'Μόνο Εικόνες';
+$lang['Incorrect file type,'] = 'Εσφαλμένος τύπος αρχείου,';
+$lang['No banner added yet'] = 'Δεν έχει προστεθεί banner ακόμα';
+$lang['Picture id.'] = 'id Εικόνας.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Βάλτε <b>%header_manager%</b>, όπου θέλετε να εμφανίσετε την εικόνα.';
+$lang['Random'] = 'Τυχαίος';
+$lang['Select an image'] = 'Επιλογή μιας εικόνας';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Ο αριθμητικός προσδιορισμός μπορεί να βρεθεί στην σελίδα έκδοσης εικόνας, κοντά στη μικρογραφία.';
+$lang['Unknown picture id'] = 'Άγνωστο αναγνωριστικό εικόνας';
+$lang['Use'] = 'Χρησιμοποιήστε';
+$lang['With text'] = 'Με κείμενο';
+$lang['You can customize the display by adding CSS rules to'] = 'Μπορείτε να προσαρμόσετε την οθόνη με την προσθήκη κανόνων CSS';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Μπορείτε να φορτώσετε μια προσαρμοσμένη κεφαλίδα εικόνας ή επιλέξτε μία από τις γκαλερί σας. Στην επόμενη οθόνη, θα είστε σε θέση να περικόψετε την εικόνα.';
+$lang['or choose a picture from the gallery'] = 'ή επιλέξτε μια εικόνα από τη συλλογή';
+?>
