db932b02222436991eab2b3697fe6444f44ae403
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-23 17:36:35 +0000

    [header_manager] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@15973 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = 'Pieviedot Baneri';
+$lang['Banner added'] = 'Baneris pievienots';
+$lang['Banner deleted'] = 'Baneris izdzēsts';
+$lang['Banner'] = 'Baneris';
+$lang['Choose an image from your computer'] = 'Izvēlaties attēlu no sava datora';
+$lang['Choose the part of the image you want to use as your header.'] = 'Izvēlaties daļu attēla, ka gribat izmantot kā galveni';
+$lang['Crop banner image'] = 'Apgriezt banera attēlu';
+$lang['Default banner size'] = 'Banera izmērs pēc noklusējuma';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Priekš MontBlancXL un BlancMontXL ieteicamais izmērs ir 900&times;190.';
+$lang['Gallery title above image'] = 'Galerijas nosaukums virs attēla';
+$lang['Image only'] = 'Tikai attēls';
+$lang['Incorrect file type,'] = 'Nekorekts faila tips';
+$lang['No banner added yet'] = 'Baneris vēl nav pievienots';
+$lang['Picture id.'] = 'Attēla id.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Liekat <b>%header_manager%</b> tur, kur gribat rādīt attēlu.';
+$lang['Random'] = 'Gadījuma';
+$lang['Select an image'] = 'Atlasīt attēlu';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Ciparveida identifikators ir atrodams attēla laidiena lapā, sīkbildes tuvumā';
+$lang['Unknown picture id'] = 'Nezināms attēla id';
+$lang['Use'] = 'Lietot';
+$lang['With text'] = 'Ar tekstu';
+$lang['You can customize the display by adding CSS rules to'] = 'Jūs varat personificēt attēlošanu, pievienojot CSS nosacījumus ';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Jūs varat augšupielādēt savu pielāgotu galvenes attēlu vai izvēlēties kādu gatavu no galerijas. Nākamajā logā būs iespējams attēlu apgriezt.';
+$lang['or choose a picture from the gallery'] = 'vai izvēlaties attēlu no galerijas';
+?>
