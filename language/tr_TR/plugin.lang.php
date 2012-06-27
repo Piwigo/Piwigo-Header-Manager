162b8c1a973df04e92265f38f8795b9f333c33d8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-06-27 19:16:25 +0000

    [header_manager] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@16095 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = 'Bir afiş (banner) ekle';
+$lang['Banner added'] = 'Afiş (banner) eklendi';
+$lang['Banner deleted'] = 'Afiş (banner) silindi';
+$lang['Banner'] = 'Afiş (Banner)';
+$lang['Choose an image from your computer'] = 'Bilgisayarınızdan bir resim seçin';
+$lang['Choose the part of the image you want to use as your header.'] = 'Başlık olarak kullanmak istediğiniz resmin parçasını seçin.';
+$lang['Crop banner image'] = 'Afiş (banner) resmini kırp';
+$lang['Default banner size'] = 'Varsayılan afiş (banner) büyüklüğü';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'MontBlancXL ve BlancMontXL için önerilen büyüklük 900&times;190 dır.';
+$lang['Gallery title above image'] = 'Galeri adı yukarıdaki resim';
+$lang['Image only'] = 'Sadece resim';
+$lang['Incorrect file type,'] = 'Geçersiz dosya tipi,';
+$lang['No banner added yet'] = 'Henüz afiş (banner) eklenmedi';
+$lang['Picture id.'] = 'Resim kimliği.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Resmi görüntülemek istediğiniz yere <b>%header_manager%</b> koyun.';
+$lang['Random'] = 'Rastgele';
+$lang['Select an image'] = 'Bir resim seçin';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Sayısal tanımlayıcı, resim düzenleme sayfasında pul resmi (thumbnail) yanından bulunabilir.';
+$lang['Unknown picture id'] = 'Bilinmeyen resim kimliği';
+$lang['Use'] = 'Kullan';
+$lang['With text'] = 'Yazı ile';
+$lang['You can customize the display by adding CSS rules to'] = 'CSS kuralları ekleyerek görününümü kişiselleştirebilirsiniz';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Bilgisayarınızdan kişisel başlık resmi yükleyebilir veya galeriden bir resmi seçebilirsiniz. Sonraki ekranda resme kırpma işlemi yapabilirsiniz.';
+$lang['or choose a picture from the gallery'] = 'yada galeriden bir resim seçin';
+?>
