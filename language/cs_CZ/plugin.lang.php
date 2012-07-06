43c4568f2b3f454902e7cecd43a5275f73c0057c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-06 21:41:28 +0000

    [header_manager] Update cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@16423 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = 'Přidat banner';
+$lang['Banner added'] = 'Banner přidán';
+$lang['Banner deleted'] = 'Banner smazán';
+$lang['Banner'] = 'Banner';
+$lang['Choose an image from your computer'] = 'Vyberte obrázek z vašeho počítače';
+$lang['Choose the part of the image you want to use as your header.'] = 'Vyberte část obrázku, kterou chcete použít jako banner';
+$lang['Crop banner image'] = 'Oříznout obrázek banneru';
+$lang['Default banner size'] = 'Standardní velikost banneru';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Pro MontBlancXL a BlancMontXL je doporučená velikost 900&times;190.';
+$lang['Gallery title above image'] = 'Nadpis galerie nad obrázkem';
+$lang['Image only'] = 'Pouze obrázek';
+$lang['Incorrect file type,'] = 'Špatný typ souboru,';
+$lang['No banner added yet'] = 'Žádný banner nebyl ještě přidán';
+$lang['Picture id.'] = 'ID obrázku';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Umístěte <b>%header_manager%</b> tam, kde chcete zobrazovat obrázek.';
+$lang['Random'] = 'Náhodně';
+$lang['Select an image'] = 'Vyberte obrázek';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Číselný identifikátor naleznete na editační stránce obrázku, poblíž náhledu.';
+$lang['Unknown picture id'] = 'Neznámé ID obrázku';
+$lang['Use'] = 'Použít';
+$lang['With text'] = 'S textem';
+$lang['You can customize the display by adding CSS rules to'] = 'Můžete přizpůsobit zobrazování přidáním CSS pravidel';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Můžete nahrát obrázek banneru z vašeho počítače, nebo použít nějaký z galerie. V dalším okně budete moct obrázek oříznout.';
+$lang['or choose a picture from the gallery'] = 'nebo vyberte obrázek z galerie';
+?>
