8b4d1daf930e594e495045d348b309a8a41e6147
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-16 06:06:56 +0000

    [header_manager] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@16764 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = 'Banner hozzáadása';
+$lang['Banner added'] = 'Banner hozzáadva';
+$lang['Banner deleted'] = 'Banner törölve';
+$lang['Banner'] = 'Banner';
+$lang['Choose an image from your computer'] = 'Kép kiválasztása a számítógépről';
+$lang['Choose the part of the image you want to use as your header.'] = 'Válassza ki a kép azon részét, amelyet a fejlécben kíván használni.';
+$lang['Crop banner image'] = 'Banner kép kivágás';
+$lang['Default banner size'] = 'Alapértelmezett banner méret';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'A MontBlancXL és BlancMontXL esetén ajánlott méret 900&times;190.';
+$lang['Image only'] = 'Csak kép';
+$lang['Incorrect file type,'] = 'Helytelen fájltípus';
+$lang['No banner added yet'] = 'Nincs még hozzáadott banner';
+$lang['Picture id.'] = 'Kép ID';
+$lang['Select an image'] = 'Kép kiválasztása';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Az ID megtalálható a kép szerkesztése oldalon, a bélyegkép mellett.';
+$lang['Use'] = 'Alkalmaz';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Töltsön fel egy fejléc képet, vagy válasszon egyet a galériából. A következő oldalon kivághatja a megfelelő méretet a képből.';
+$lang['or choose a picture from the gallery'] = 'vagy válasszon egy képet a galériából';
+?>
