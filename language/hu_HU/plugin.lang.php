caebddea53a71c5a17a7676b0e63961b797ed270
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-17 05:37:36 +0000

    [header_manager] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@16796 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -30,7 +30,7 @@ $lang['Crop banner image'] = 'Banner kép kivágás';
 $lang['Default banner size'] = 'Alapértelmezett banner méret';
 $lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'A MontBlancXL és BlancMontXL esetén ajánlott méret 900&times;190.';
 $lang['Image only'] = 'Csak kép';
-$lang['Incorrect file type,'] = 'Helytelen fájltípus';
+$lang['Incorrect file type,'] = 'Helytelen fájltípus.';
 $lang['No banner added yet'] = 'Nincs még hozzáadott banner';
 $lang['Picture id.'] = 'Kép ID';
 $lang['Select an image'] = 'Kép kiválasztása';
@@ -38,4 +38,10 @@ $lang['The numeric identifier can be found on the picture edition page, near the
 $lang['Use'] = 'Alkalmaz';
 $lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Töltsön fel egy fejléc képet, vagy válasszon egyet a galériából. A következő oldalon kivághatja a megfelelő méretet a képből.';
 $lang['or choose a picture from the gallery'] = 'vagy válasszon egy képet a galériából';
+$lang['Gallery title above image'] = 'Galéria címe a képben';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Tegye a <b>%header_manager%</b> kifejezést oda, ahol a képet meg kívánja jeleníteni.';
+$lang['Random'] = 'Véletlen';
+$lang['Unknown picture id'] = 'Ismeretlen kép ID';
+$lang['With text'] = 'Csak szöveg';
+$lang['You can customize the display by adding CSS rules to'] = 'A megjelenítés testreszabható a CSS szabályok megadásával';
 ?>
