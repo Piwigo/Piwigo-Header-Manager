6cc472c512ec492a295a46cae08318ec058e8267
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-21 11:11:26 +0000

    [header_manager] Update nl_NL, thanks to : ruud
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@16884 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = 'Voeg een banier toe';
+$lang['Banner added'] = 'Banier toegevoegd';
+$lang['Banner deleted'] = 'Banier verwijderd';
+$lang['Banner'] = 'Banier';
+$lang['Choose an image from your computer'] = 'Kies een afbeelding op je computer';
+$lang['Choose the part of the image you want to use as your header.'] = 'Kies het deel van de afbeelding dat je wilt gebruiken';
+$lang['Crop banner image'] = 'Banierafbeelding afsnijden';
+$lang['Default banner size'] = 'Standaardbaniergrootte';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Voor MontBlancXL en BlancMontXL is de aanbevolen grootte 900&times;190.';
+$lang['Gallery title above image'] = 'Titel van de galerij boven de afbeelding';
+$lang['Image only'] = 'Alleen afbeelding';
+$lang['Incorrect file type,'] = 'Fout bestandstype';
+$lang['No banner added yet'] = 'Nog geen banier toegevoegd';
+$lang['Picture id.'] = 'Afbeeldings-id';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Plaat <b>%header_manager%</b> op de plek waar je de afbeelding wilt tonen.';
+$lang['Random'] = 'Willekeurig';
+$lang['Select an image'] = 'Kies een afbeelding';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'De numerieke identificatie kun je vinden op de afbeeldingsbewerkingspagina, bij de verkleinde weergave.';
+$lang['Unknown picture id'] = 'Onbekende afbeeldings-id';
+$lang['Use'] = 'Gebruik';
+$lang['With text'] = 'Met tekst';
+$lang['You can customize the display by adding CSS rules to'] = 'Je kunt het venster aanpassen door CSS-regels toe te voegen aan ';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Je kunt een aangepaste banierafbeelding inlezen of er een kiezen uit je galerij. In het volgende scherm kun je de afbeelding bijsnijden.';
+$lang['or choose a picture from the gallery'] = 'of kies een afbeelding uit de galerij';
+?>
