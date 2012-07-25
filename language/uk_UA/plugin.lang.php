7ed855bd7cc5f01f6a486b51bf41a22639a1ed18
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-25 06:36:17 +0000

    [header_manager] Update uk_UA, thanks to : Rata
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@16996 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/uk_UA/plugin.lang.php
@@ -0,0 +1,48 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = 'Додати банер';
+$lang['Banner added'] = 'Банер додано';
+$lang['Banner deleted'] = 'Банер видалено';
+$lang['Banner'] = 'Банер';
+$lang['Choose an image from your computer'] = 'Оберіть файл із зображенням';
+$lang['Choose the part of the image you want to use as your header.'] = 'Оберіть область зображення, яку Ви бажаєте використати у якості заголовка.';
+$lang['Crop banner image'] = 'Обрізати зображення банера';
+$lang['Default banner size'] = 'Розмір банера за умовчанням';
+$lang['Default banner'] = 'Стандартный баннер';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Рекомендований розмір для тем оформлення MontBlancXL та BlancMontXL 900&times;190.';
+$lang['Gallery title above image'] = 'Назва галереї над зображенням';
+$lang['Image only'] = 'Тільки зображення';
+$lang['Incorrect file type,'] = 'Невірний тип файлу,';
+$lang['No banner added yet'] = 'Банер не додано';
+$lang['Picture id.'] = 'Ідентифікатор зображення';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Додайте тег <b>% header_manager% </ b> у тому місці, де має виводитися зображення.';
+$lang['Random'] = 'Випадково';
+$lang['Select an image'] = 'Оберіть зображення';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Цифровий ідентифікатор зображення знаходиться на сторінці редагування біля мініатюри';
+$lang['Unknown picture id'] = 'Невідомий ідентифікатор зображення';
+$lang['Use'] = 'Використовувати';
+$lang['With text'] = 'З текстом';
+$lang['You can customize the display by adding CSS rules to'] = 'Ви можете налаштувати відображення за допомогою стилів CSS';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Ви можете завантажити зображення користувача або вибрати картинку з галереї. На наступній сторінці Ви зможете обрізати вибране зображення.';
+$lang['or choose a picture from the gallery'] = 'або обрати зображення з галереї';
+?>
