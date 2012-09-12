ebc786f5102fd65cfb8ee678b98820f2adfe8a19
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-12 04:41:53 +0000

    [header_manager] Update zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@17870 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/zh_CN/plugin.lang.php
+++ language/zh_CN/plugin.lang.php
@@ -20,9 +20,30 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Add a banner'] = '添加一个横幅';
-$lang['Banner'] = '横幅';
-$lang['Banner added'] = '已添加横幅';
-$lang['Banner deleted'] = '已删除横幅';
-$lang['Choose an image from your computer'] = '从您的计算机选择一个图像';
+$lang['Add a banner'] = '添加一个banner';
+$lang['Banner'] = 'Banner';
+$lang['Banner added'] = 'Banner已添加';
+$lang['Banner deleted'] = 'Banner已删除';
+$lang['Choose an image from your computer'] = '从您的电脑中选择一张图片';
+$lang['Choose the part of the image you want to use as your header.'] = '选择图片中您想用作banner的部分。';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = '图片的数字id可在图片编辑页面靠近缩略图处找到。';
+$lang['Unknown picture id'] = '未知图片id';
+$lang['Use'] = '使用';
+$lang['With text'] = '包括文字';
+$lang['You can customize the display by adding CSS rules to'] = '您可以通过添加CSS规则的方式自定显示效果 ';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = '您可以上传一张图片或从您的相册中选择一张图片，在下一页您可以对图片进行裁切。';
+$lang['or choose a picture from the gallery'] = '或从相册中选择一张图片';
+$lang['Crop banner image'] = '裁切banner图片';
+$lang['Default banner'] = '默认banner';
+$lang['Default banner size'] = '默认banner尺寸';
+$lang['Display banner on photo page'] = '在图片页面上显示banner';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = '对于 MontBlancXL 和 BlancMontXL，建议使用的尺寸是 900&×;190。';
+$lang['Gallery title above image'] = '图片上方的相册标题';
+$lang['Image only'] = '仅图片';
+$lang['Incorrect file type,'] = '文件类型错误，';
+$lang['No banner added yet'] = '尚无已添加的banner';
+$lang['Picture id.'] = '图片id。';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = '在您想显示图片的地方放置 <b>%header_manager%</b> 。';
+$lang['Random'] = '随机';
+$lang['Select an image'] = '选择一张图片';
 ?>
