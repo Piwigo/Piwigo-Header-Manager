20bac2b3bed76b165b681e104089e29e07203ed0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-18 18:57:52 +0000

    [header_manager] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@18002 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Unknown picture id'] = 'Ukendt billedid.';
+$lang['Use'] = 'Anvend';
+$lang['With text'] = 'Med tekst';
+$lang['You can customize the display by adding CSS rules to'] = 'Du kan tilpasse visningen ved at tilføje CSS-regler til';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Du kan uploade et tilpasset headerbillede eller vælge et fra dit galleri. På den næste side får du mulighed for at beskære billedet.';
+$lang['or choose a picture from the gallery'] = 'eller vælg et billede fra galleriet';
+$lang['Add a banner'] = 'Tilføj et banner';
+$lang['Banner'] = 'Banner';
+$lang['Banner added'] = 'Banner tilføjet';
+$lang['Banner deleted'] = 'Banner slettet';
+$lang['Choose an image from your computer'] = 'Vælg et billede på din computer';
+$lang['Choose the part of the image you want to use as your header.'] = 'Vælg den del af billedet, du ønsker at anvende som header.';
+$lang['Crop banner image'] = 'Beskær bannerbilledet';
+$lang['Default banner'] = 'Standardbanner';
+$lang['Default banner size'] = 'Standardbanners størrelse';
+$lang['Display banner on photo page'] = 'Vis banner på fotoside';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'For MontBlancXL og BlancMontXL er den anbefalede størrelse 900&times;190.';
+$lang['Gallery title above image'] = 'Galleris titel over billede';
+$lang['Image only'] = 'Kun billede';
+$lang['Incorrect file type,'] = 'Ukorrekt filtype';
+$lang['No banner added yet'] = 'Banner endnu ikke tilføjet';
+$lang['Picture id.'] = 'Billedid.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Placer <b>%header_manager%</b> hvor du ønsker at vise billedet.';
+$lang['Random'] = 'Tilfældigt';
+$lang['Select an image'] = 'Vælg et billede';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Den numeriske identifikation finder man på billedredigeringssiden, tæt på miniaturebilledet.';
+?>
