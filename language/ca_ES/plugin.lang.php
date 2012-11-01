dcb4d5174fdd64fcf2702d3d6e822daade1b05c4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-11-01 16:48:05 +0000

    [header_manager] Update ca_ES, thanks to : josep.torne
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@18859 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Per MontBlancXL i BlancMontXL, es recomana una mida de 900&times;190';
+$lang['Gallery title above image'] = 'Títol de la galeria al costat de la imatge';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Podeu penjar una capçalera personalitzada o seleccionar-ne una de la galeria. En la següent pantalla podreu escapçar la imatge.';
+$lang['Image only'] = 'Només la imatge';
+$lang['Unknown picture id'] = 'Id de la imatge desconegut';
+$lang['Use'] = 'Utilitza';
+$lang['With text'] = 'Amb text';
+$lang['You can customize the display by adding CSS rules to'] = 'Podeu personalitzar la viualització afegint regles CSS';
+$lang['or choose a picture from the gallery'] = 'o escolliu una imatge de la galeria';
+$lang['Add a banner'] = 'Afegeix un bàner';
+$lang['Banner'] = 'Bàner';
+$lang['Banner added'] = 'El bàner s\'ha afegit';
+$lang['Banner deleted'] = 'El bàner s\'ha eliminat';
+$lang['Choose an image from your computer'] = 'Escolliu una imatge del vostre ordinador';
+$lang['Choose the part of the image you want to use as your header.'] = 'Escolliu la part de la imatge que voleu utilitzar com a capçalera.';
+$lang['Crop banner image'] = 'Escapça la imatge de bàner';
+$lang['Default banner'] = 'Bàner predeterminat';
+$lang['Default banner size'] = 'Mida del bàner predeterminada';
+$lang['Display banner on photo page'] = 'Mostra el bàner sobre la pàgina de les fotos';
+$lang['Incorrect file type,'] = 'Tipus de fitxer incorrecte,';
+$lang['No banner added yet'] = 'Encara no s\'ha afegit cap bàner';
+$lang['Picture id.'] = 'Id de la imatge.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Poseu <b>%header_manager%</b> allí on vulgueu mostrar la imatge';
+$lang['Random'] = 'Aleatori';
+$lang['Select an image'] = 'Seleccioneu una imatge';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'L\'identificador numèric es pot trobar a la pàgina d\'edició de la imatge, al costat de la miniatura.';
+?>
