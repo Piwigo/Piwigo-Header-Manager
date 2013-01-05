918a3f21dae0564481ca1ba71e4d1ff18ce1d408
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-05 16:09:02 +0000

    [header_manager] Add et_EE, thanks to : kersta
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@19852 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,46 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Unknown picture id'] = 'Tundmatu pildi id-number';
+$lang['Use'] = 'Kasuta';
+$lang['With text'] = 'Tekstiga';
+$lang['Add a banner'] = 'Lisa bänner';
+$lang['Banner'] = 'Bänner';
+$lang['Banner added'] = 'Bänner lisatud';
+$lang['Banner deleted'] = 'Bänner kustutatud';
+$lang['Choose an image from your computer'] = 'Vali pilt oma arvutist';
+$lang['Choose the part of the image you want to use as your header.'] = 'Vali osa pildist, mida tahad kasutada päisena';
+$lang['Crop banner image'] = 'Kärbi bänneri pilti';
+$lang['Default banner'] = 'Vaikimisi valitud bänner';
+$lang['Default banner size'] = 'Vaikimisi valitud bänneri suurus';
+$lang['Display banner on photo page'] = 'Näita bännerit fotode lehel';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'MontBlancXL ja BlancMontXL jaoks soovituslik suurus 900x190';
+$lang['Gallery title above image'] = 'Galerii pealkiri pildi kohal';
+$lang['Image only'] = 'Ainult pilt';
+$lang['Incorrect file type,'] = 'Ebakorrektne faili tüüp';
+$lang['No banner added yet'] = 'Bännerit ei ole lisatud veel';
+$lang['Picture id.'] = 'Pildi id-number';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Aseta %header_manager% kohta kus soovid pilti kuvada';
+$lang['Random'] = 'Juhuslik';
+$lang['Select an image'] = 'Vali pilt';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Id-numbri leiad piltide toimetamise lehelt, pisipildi kõrvalt';
+?>
