81b690c9709b185d0d9b843212634b8747483053
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-07 05:39:58 +0000

    [header_manager] Add pt_BR, thanks to : oigreslima
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@19933 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Picture id.'] = 'ID da foto';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Ponha <b>%header_manager%</b> onde você quer mostrar a imagem.';
+$lang['Random'] = 'Aleatório';
+$lang['Select an image'] = 'Selecione uma imagem';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'O identificador numérico pode ser encontrado na página de edição da foto, próximo a miniatura da mesma.';
+$lang['Unknown picture id'] = 'Id da foto desconhecido.';
+$lang['Use'] = 'Use';
+$lang['With text'] = 'Com texto';
+$lang['You can customize the display by adding CSS rules to'] = 'Você pode personalizar a exibição adicionado regras de CSS';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Você pode enviar uma imagem personalizada do cabeçalho ou selecionar uma de seus albuns. Na próxima tela você poderá recortar a imagem.';
+$lang['or choose a picture from the gallery'] = 'ou escolha uma foto dos seus álbuns';
+$lang['Default banner size'] = 'Tamanho padrão para o Banner';
+$lang['Display banner on photo page'] = 'Mostra banner na página de foto';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Para MontBlanXL e BlanMontXL, aconselha-se o tamanho 900&times;190.';
+$lang['Gallery title above image'] = 'Título do Álbum abaixo da Imagem';
+$lang['Image only'] = 'Imagem somente';
+$lang['Incorrect file type,'] = 'Tipo de arquivo incorreto';
+$lang['No banner added yet'] = 'Nenhum banner adicionado ainda';
+$lang['Add a banner'] = 'Adicione um Banner';
+$lang['Banner'] = 'Banner';
+$lang['Banner added'] = 'Banner adicionado';
+$lang['Banner deleted'] = 'Banner apagado';
+$lang['Choose an image from your computer'] = 'Escolha uma imagem do seu computador';
+$lang['Choose the part of the image you want to use as your header.'] = 'Escolha a parte da imagem que você quer usar no seu cabeçalho.';
+$lang['Crop banner image'] = 'Recorta a imagem do Banner';
+$lang['Default banner'] = 'Banner padrão';
+?>
