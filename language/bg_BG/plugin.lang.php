2a98655a88f80367c3dde2e508632c6c54f32e18
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-10 15:10:14 +0000

    [header_manager] Update bg_BG, thanks to : turnokopa
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@20042 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,49 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['or choose a picture from the gallery'] = 'или изберете снимка от галерията';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Може да качите лична снимка за заглавие или да изберете някоя от галерията. На следващия екран ще може да преоръзмерите снимката.';
+$lang['You can customize the display by adding CSS rules to'] = 'Може да управляваш изгледа си с добавяне на CSS правила';
+$lang['With text'] = 'С текст';
+$lang['Use'] = 'Използвай';
+$lang['Unknown picture id'] = 'Непознат номер снимка';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Цифравият индикатор може да бъде намерен на страницата за редакция на снимки, близо до миниатюрата.';
+$lang['Select an image'] = 'Избери снимка';
+$lang['Random'] = 'Пройзволно';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Сложи <b>%header_manager%</b> където желаеш да покажеш снимка.';
+$lang['Picture id.'] = 'Номер на снимка';
+$lang['No banner added yet'] = 'Още не е добавен банер';
+$lang['Incorrect file type,'] = 'Неточен файлов тип';
+$lang['Image only'] = 'Само снимка';
+$lang['Gallery title above image'] = 'Заглавие отгоре над снимката в Галерия';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'За MontBlancXL и BlancMontXL, разширен размер е 900&times;190.';
+$lang['Display banner on photo page'] = 'Показване на банер на страниците със снимки';
+$lang['Default banner size'] = 'Размер по подразбиране за банер';
+$lang['Default banner'] = 'Банер по подразбиране';
+$lang['Crop banner image'] = 'Преоразмеряване на банер';
+$lang['Choose the part of the image you want to use as your header.'] = 'Избери коя част от снимката искаш да ползваш за заглавие';
+$lang['Choose an image from your computer'] = 'Избири снимка от компютъра си';
+$lang['Banner deleted'] = 'Банерът е изтрит';
+$lang['Banner added'] = 'Банерът е добавен';
+$lang['Banner'] = 'Банер';
+$lang['Add a banner'] = 'Добави банер';
+?>
