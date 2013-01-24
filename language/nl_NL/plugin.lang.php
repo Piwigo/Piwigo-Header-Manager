4e4573386a233ee6822f680f599351b7a046e3d0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-24 12:59:12 +0000

    [header_manager] Update nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@20354 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -20,30 +20,35 @@
 // | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
-$lang['Add a banner'] = 'Voeg een banier toe';
-$lang['Banner added'] = 'Banier toegevoegd';
-$lang['Banner deleted'] = 'Banier verwijderd';
-$lang['Banner'] = 'Banier';
-$lang['Choose an image from your computer'] = 'Kies een afbeelding op je computer';
-$lang['Choose the part of the image you want to use as your header.'] = 'Kies het deel van de afbeelding dat je wilt gebruiken';
-$lang['Crop banner image'] = 'Banierafbeelding afsnijden';
-$lang['Default banner size'] = 'Standaardbaniergrootte';
+$lang['Add a banner'] = 'Voeg een banner toe';
+$lang['Banner added'] = 'Banner toegevoegd';
+$lang['Banner deleted'] = 'Banner verwijderd';
+$lang['Banner'] = 'Banner';
+$lang['Choose an image from your computer'] = 'Kies een afbeelding op uw computer';
+$lang['Choose the part of the image you want to use as your header.'] = 'Kies het deel van de afbeelding dat u wilt gebruiken.';
+$lang['Crop banner image'] = 'Banner-afbeelding bijsnijden';
+$lang['Default banner size'] = 'Standaard bannergrootte';
 $lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Voor MontBlancXL en BlancMontXL is de aanbevolen grootte 900&times;190.';
-$lang['Gallery title above image'] = 'Titel van de galerij boven de afbeelding';
+$lang['Gallery title above image'] = 'Titel van de galerie boven de afbeelding';
 $lang['Image only'] = 'Alleen afbeelding';
-$lang['Incorrect file type,'] = 'Fout bestandstype';
-$lang['No banner added yet'] = 'Nog geen banier toegevoegd';
+$lang['Incorrect file type,'] = 'Fout bestandstype.';
+$lang['No banner added yet'] = 'Nog geen banner toegevoegd';
 $lang['Picture id.'] = 'Afbeeldings-id';
-$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Plaat <b>%header_manager%</b> op de plek waar je de afbeelding wilt tonen.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Plaat <b>%header_manager%</b> op de plek waar u de afbeelding wilt tonen.';
 $lang['Random'] = 'Willekeurig';
 $lang['Select an image'] = 'Kies een afbeelding';
-$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'De numerieke identificatie kun je vinden op de afbeeldingsbewerkingspagina, bij de verkleinde weergave.';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'De numerieke identificatie kun je vinden op de afbeeldingsbewerkingspagina, in de buurt van het klikplaatje.';
 $lang['Unknown picture id'] = 'Onbekende afbeeldings-id';
 $lang['Use'] = 'Gebruik';
 $lang['With text'] = 'Met tekst';
-$lang['You can customize the display by adding CSS rules to'] = 'Je kunt het venster aanpassen door CSS-regels toe te voegen aan ';
-$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Je kunt een aangepaste banierafbeelding inlezen of er een kiezen uit je galerij. In het volgende scherm kun je de afbeelding bijsnijden.';
-$lang['or choose a picture from the gallery'] = 'of kies een afbeelding uit de galerij';
-$lang['Default banner'] = 'standaardbanier';
-$lang['Display banner on photo page'] = 'Geef de banier weer op de foto-pagina';
+$lang['You can customize the display by adding CSS rules to'] = 'U kunt het venster aanpassen door CSS-regels toe te voegen aan ';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'U kunt een aangepaste bannerafbeelding uploaden of een afbeelding kiezen uit uw galerie. In het volgende scherm kunt u de afbeelding bijsnijden.';
+$lang['or choose a picture from the gallery'] = 'of kies een afbeelding uit de galerie';
+$lang['Default banner'] = 'Standaard banner';
+$lang['Display banner on photo page'] = 'Geef de banner weer op de foto-pagina';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'Om een specifieke banner toe te kunnen voegen, gaat u naar de beheerpagina van het gewenste album.';
+$lang['Non recursive'] = 'Niet herhalend';
+$lang['Recursive'] = 'Herhalend';
+$lang['Restore default banner'] = 'Terug naar de standaardinstellingen van de banner';
+$lang['Album specific banners'] = 'Album specifieke banner';
 ?>
