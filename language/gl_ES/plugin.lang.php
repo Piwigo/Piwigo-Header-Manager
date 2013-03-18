f7046bae8608923de620748e59761ce476611218
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-18 20:38:32 +0000

    [header_manager] Add gl_ES, thanks to : damufo
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@21684 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/gl_ES/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Podes cargar unha imaxe personalizada para a cabeceira ou seleccionar unha da túa galería. A seguinte pantalla permíteche recortar a imaxe.';
+$lang['Random'] = 'Ó chou';
+$lang['Recursive'] = 'Recursivo';
+$lang['Restore default banner'] = 'Restaurar o cartel predeterminado';
+$lang['Select an image'] = 'Seleccionar unha imaxe';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'O identificador numérico da imaxe pode atoparse na páxina de modificación da imaxe, perto da miniatura.';
+$lang['Unknown picture id'] = 'O identificador da imaxe é descoñecido';
+$lang['Use'] = 'Uso';
+$lang['With text'] = 'Con texto';
+$lang['You can customize the display by adding CSS rules to'] = 'Podes personalizar a pantalla engadindo regras CSS a';
+$lang['or choose a picture from the gallery'] = 'ou escolle unha imaxe da túa galería';
+$lang['Crop banner image'] = 'Recorta-la imaxe do cartel';
+$lang['Display banner on photo page'] = 'Amosa-lo cartel na páxina da foto';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'Para poder engadir un cartel específico, vai á páxina de administración do álbum desexado.';
+$lang['Incorrect file type,'] = 'Tipo de ficheiro incorrecto';
+$lang['No banner added yet'] = 'Aínda non hai ningún cartel';
+$lang['Non recursive'] = 'Non recursivo';
+$lang['Picture id.'] = 'Identificador da foto';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Pon  <b>%header_manager%</b> onde queiras amosar a imaxe.';
+$lang['Default banner'] = 'Cartel predeterminado';
+$lang['Default banner size'] = 'Tamaño do cartel predeterminado';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Para MontBlancXL e BlancMontXL, o tamaño recomendado é de 900&times;190.';
+$lang['Gallery title above image'] = 'Título da galería sobre a imaxe';
+$lang['Image only'] = 'Só imaxe';
+$lang['Choose the part of the image you want to use as your header.'] = 'Escolle a parte da imaxe que queiras usar como cabeceira.';
+$lang['Add a banner'] = 'Engadir un cartel';
+$lang['Album specific banners'] = 'Carteis específicos do álbum';
+$lang['Banner'] = 'Cartel';
+$lang['Banner added'] = 'Engadiuse o cartel';
+$lang['Banner deleted'] = 'Eliminouse o cartel';
+$lang['Choose an image from your computer'] = 'Escolle unha imaxe para o teu computador';
+?>
