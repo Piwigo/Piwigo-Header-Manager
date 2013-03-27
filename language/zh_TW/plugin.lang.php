7a5dd23a6d9965fbecb5f58c82235a8a495c7a39
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-27 19:59:54 +0000

    [header_manager] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@21878 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = '新增一個橫幅廣告';
+$lang['Album specific banners'] = '相簿特定的橫幅廣告';
+$lang['Banner'] = '橫幅廣告';
+$lang['Banner added'] = '橫幅廣告已增加';
+$lang['Banner deleted'] = '橫幅廣告已刪除';
+$lang['Choose an image from your computer'] = '請從您的電腦中選擇一個圖片';
+$lang['Choose the part of the image you want to use as your header.'] = '選擇部分的圖片，你要使用你標題。';
+$lang['Crop banner image'] = '裁切橫幅廣告圖片';
+$lang['Default banner'] = '預設橫幅廣告';
+$lang['Default banner size'] = '預設橫幅廣告尺寸';
+$lang['Display banner on photo page'] = '顯示橫幅廣告在圖片頁上';
+?>
