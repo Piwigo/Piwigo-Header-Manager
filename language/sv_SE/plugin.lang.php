6fdde909871ad0fcf937cca9e08f415bfc593ae7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-28 15:49:28 +0000

    [header_manager] Add sv_SE, thanks to : stefan63
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@22391 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Random'] = 'Slumpmässig';
+$lang['Recursive'] = 'Rekursiv';
+$lang['Restore default banner'] = 'Återsäll standard banner';
+$lang['Select an image'] = 'Välj en bild';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Numerisk bild id kan hittas på bildupplage sidan, nära thumbnail bilden.';
+$lang['Unknown picture id'] = 'Okänt bild id';
+$lang['Use'] = 'Använd';
+$lang['With text'] = 'Med text';
+$lang['You can customize the display by adding CSS rules to'] = 'Du kan anpassa utseendet genom att lägga till CSS regler till';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Du kan ladda upp en anpassad sidhuvuds bild eller välja en från ditt album. I nästa steg kan du beskära bilden.';
+$lang['or choose a picture from the gallery'] = 'eller välja en bild från albumet';
+$lang['Add a banner'] = 'Lägg till en banner';
+$lang['Album specific banners'] = 'Banners anpassade till album';
+$lang['Banner'] = 'Banner';
+$lang['Banner added'] = 'Banner tillagd';
+$lang['Banner deleted'] = 'Banner borttagen';
+$lang['Choose an image from your computer'] = 'Välj bild från din dator';
+$lang['Choose the part of the image you want to use as your header.'] = 'Välj den del av bilden som du vill hanvända som sidhuvud.';
+$lang['Crop banner image'] = 'Beskär banner bild';
+$lang['Default banner'] = 'Standard banner';
+$lang['Default banner size'] = 'Standardstorlek på banner';
+$lang['Display banner on photo page'] = 'Visa banner på bild sida';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Rekomenderad storlek För MontBlancXL och BlancMontXL är,  900& X ;190.';
+$lang['Gallery title above image'] = 'Album titel ovanför bilden';
+$lang['Image only'] = 'Endast bild';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'För att lägga till en specifik banner, gå till admin sidan för det önskade albummet';
+$lang['Incorrect file type,'] = 'Fel filtyp,';
+$lang['No banner added yet'] = 'Ingen banner tillagd ännu';
+$lang['Non recursive'] = 'Icke-rekursiv';
+$lang['Picture id.'] = 'Bild id.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Placera <b>%header_manager%</b> där du vill att bilden visas.';
+?>
