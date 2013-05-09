d1516ab02b7c2ef8f1e76cdcf35983bfa8d5bf83
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-09 05:06:48 +0000

    [header_manager] Add sl_SI, thanks to : MarkoAm
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@22564 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Random'] = 'Naključno';
+$lang['Recursive'] = 'Ponavljajoče';
+$lang['Restore default banner'] = 'Obnovi privzeto pasico';
+$lang['Select an image'] = 'Izberi sliko';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Številčno oznako lahko najdete na strani urejanja slike, v bližini sličice';
+$lang['Unknown picture id'] = 'Neznan ID slike';
+$lang['Use'] = 'Uporabi';
+$lang['With text'] = 'Z besedilom';
+$lang['You can customize the display by adding CSS rules to'] = 'Prikaz lahko prilagodite z dodajanjem CSS pravila';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Naložite lahko sliko glave po meri ali pa izberete sliko iz galerije. Na naslednji strani boste sliko lahko obrezali.';
+$lang['or choose a picture from the gallery'] = 'ali izberi sliko iz galerije';
+$lang['Add a banner'] = 'Dodaj pasico';
+$lang['Album specific banners'] = 'Posebne pasice albuma';
+$lang['Banner'] = 'Pasica';
+$lang['Banner added'] = 'Pasica dodana';
+$lang['Banner deleted'] = 'Pasica odstranjena';
+$lang['Choose an image from your computer'] = 'Izbiri sliko iz računalnika';
+$lang['Choose the part of the image you want to use as your header.'] = 'Izberite del slike, ki jo želite uporabiti kot glavo';
+$lang['Crop banner image'] = 'Obreži sliko pasice';
+$lang['Default banner'] = 'Privzeta pasica';
+$lang['Default banner size'] = 'Privzeta velikost pasice';
+$lang['Display banner on photo page'] = 'Prikaži pasico na strani s slikami';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Za MontBlancXL in BlancMontXL je priporočena velikost 900&krat;190.';
+$lang['Gallery title above image'] = 'Naslov galerije nad sliko';
+$lang['Image only'] = 'Samo slika';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'Da bi dodali določeno pasico, pojdite na admin stran željenega albuma.';
+$lang['Incorrect file type,'] = 'Nepravilna vrsta datoteke';
+$lang['No banner added yet'] = 'Nobena pasica še ni dodana';
+$lang['Non recursive'] = 'Brez ponavljanja';
+$lang['Picture id.'] = 'ID slike';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Vstavi <b>%upravitelja_glave%</b> kjer želiš prikazati sliko.';
+?>
