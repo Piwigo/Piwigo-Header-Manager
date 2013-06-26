e60ca1b7e3f526552fe7f7eec65d516b310c1569
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-26 04:22:05 +0000

    [header_manager] Add zh_HK, thanks to : gurunited
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@23556 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_HK/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = '您可以上傳自定標題圖片，或從您的畫廊中選擇。在下個畫面，您可以裁剪圖片。';
+$lang['or choose a picture from the gallery'] = '或由畫廊中揀選一張圖片';
+$lang['Recursive'] = '重覆';
+$lang['Restore default banner'] = '還原為預設橫額';
+$lang['Select an image'] = '選擇一張圖片';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = '數字識別碼可以在圖片編輯頁面找到, 在縮圖畫面附近。';
+$lang['You can customize the display by adding CSS rules to'] = '你可以增加CSS的規則而更改顯示畫面';
+$lang['Add a banner'] = '新增一個橫額';
+$lang['Album specific banners'] = '相簿特定的橫額';
+$lang['Banner'] = '橫額';
+$lang['Banner added'] = '橫額已增加';
+$lang['Banner deleted'] = '橫額已刪除';
+$lang['Choose an image from your computer'] = '從電腦中選擇一張圖片';
+$lang['Choose the part of the image you want to use as your header.'] = '選擇部分圖片作為你的標題。';
+$lang['Crop banner image'] = '裁切橫額圖片';
+$lang['Default banner'] = '預設橫額';
+$lang['Default banner size'] = '預設橫額尺寸';
+$lang['Display banner on photo page'] = '顯示橫額在圖片頁上';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = '如選用MontBlancXL和BlancMontXL，建議大小為900×190。';
+$lang['Gallery title above image'] = '畫廊標題設於圖片上方';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = '如要新增特定的橫額，請前往相冊的管理頁面。';
+$lang['Incorrect file type,'] = '錯誤的檔案類型，';
+$lang['No banner added yet'] = '尚未新增橫額';
+$lang['Non recursive'] = '非重覆的';
+$lang['Picture id.'] = '圖片ID';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = '把 <b>%header_manager%</b> 放在你要顯示的圖像。';
+$lang['Random'] = '隨機';
+$lang['Unknown picture id'] = '未知的圖片ID';
+$lang['Use'] = '使用';
+$lang['With text'] = '帶有文字';
+$lang['Image only'] = '只有圖片';
+?>
