0340de192d758c3daa743fbc46083a6d13e597af
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-27 04:45:13 +0000

    [header_manager] Add vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@24269 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Random'] = 'Ngẫu nhiên';
+$lang['Recursive'] = 'Đệ quy';
+$lang['Restore default banner'] = 'Khôi phục banner mặc định';
+$lang['Select an image'] = 'Chọn một ảnh';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Số định danh có thể tìm thấy trong trang biên tập hình, gần với ảnh thu nhỏ.';
+$lang['Unknown picture id'] = 'Định danh ảnh không xác định';
+$lang['Use'] = 'Dùng';
+$lang['With text'] = 'Với văn bản';
+$lang['You can customize the display by adding CSS rules to'] = 'Bạn có thể điều chỉnh việc hiển thị bằng cách thêm các quy định CSS vào';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Bạn có thể upload một ảnh header tùy chọn hoặc chọn từ gallery cá nhân. Trong màn hình tiếp theo bạn có thể cắt gọt hình.';
+$lang['or choose a picture from the gallery'] = 'hoặc chọn một ảnh từ gallery';
+$lang['Add a banner'] = 'Thêm một banner';
+$lang['Album specific banners'] = 'Các banner cụ thể của album';
+$lang['Banner'] = 'Banner';
+$lang['Banner added'] = 'Banner đã được thêm';
+$lang['Banner deleted'] = 'Banner đã được xóa';
+$lang['Choose an image from your computer'] = 'Chọn một ảnh từ máy tính';
+$lang['Choose the part of the image you want to use as your header.'] = 'Chọn phần của hình ảnh mà bạn muốn dùng để làm header';
+$lang['Crop banner image'] = 'Cắt ảnh banner';
+$lang['Default banner'] = 'Banner mặc định';
+$lang['Default banner size'] = 'Kích thước banner mặc định';
+$lang['Display banner on photo page'] = 'Hiển thị banner trong trang ảnh';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Đối với MontBlancXL và BlancMontXL, kích thước khuyến cáo là 900&times;190.';
+$lang['Gallery title above image'] = 'Tiêu đề gallery nằm trên ảnh';
+$lang['Image only'] = 'Chỉ hình ảnh';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'Để thêm một banner cụ thể, hãy vào trang quản trị của album đó';
+$lang['Incorrect file type,'] = 'Loại file không đúng,';
+$lang['No banner added yet'] = 'Chưa có banner được thêm';
+$lang['Non recursive'] = 'Không đệ quy';
+$lang['Picture id.'] = 'Định danh ảnh';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Đặt <b>%header_manager%</b>tại nơi muốn hiển thị ảnh.';
+?>
