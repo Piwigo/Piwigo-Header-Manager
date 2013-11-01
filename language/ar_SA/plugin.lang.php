cf0f510473b79c560b79d9e94edb7f477c34d1c7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-01 14:43:24 +0000

    [header_manager] Update ar_SA, thanks to : aldambi
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@25284 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_SA/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['With text'] = 'مع النص';
+$lang['or choose a picture from the gallery'] = 'أو اختيار صورة من المعرض';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'يمكنك تحميل صورة ترويسة خاصة أو اختيار واحدة من معرض الصور الخاص بك. تستطيع اقتصاص الصورة على الشاشة التالية.';
+$lang['You can customize the display by adding CSS rules to'] = 'يمكنك تخصيص الشاشة عن طريق إضافة قواعد CSS لـ';
+$lang['Use'] = 'استخدم';
+$lang['Unknown picture id'] = 'معرف الصورة مجهول';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'المعرف رقمي يمكن العثور عليه على صفحة صورة الطبعة، بالقرب المصغرات.';
+$lang['Select an image'] = 'تحديد صورة';
+$lang['Restore default banner'] = 'استعادة الافتة الافتراضية';
+$lang['Recursive'] = 'تكراري';
+$lang['Random'] = 'عشوائي';
+$lang['Default banner size'] = 'حجم اللافتة الافتراضي';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'وضع <b>%header_manager%</b> حيث كنت ترغب في عرض الصورة.';
+$lang['Picture id.'] = 'معرف الصورة.';
+$lang['Non recursive'] = 'غير المكررة';
+$lang['No banner added yet'] = 'لم تضاف اي لافتة حتى الان';
+$lang['Incorrect file type,'] = 'نوع الملف غير صحيح،';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'لإضافة لافتة معينة، انتقل إلى صفحة الإدارة من الألبوم المرغوب.';
+$lang['Image only'] = 'صورة فقط';
+$lang['Gallery title above image'] = 'عنوان المعرض اعلى الصورة';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'لـ MontBlancXL و  BlancMontXL، الحجم الموصى به 900 × 190.';
+$lang['Display banner on photo page'] = 'عرض الافتة على صفحة الصور';
+$lang['Default banner'] = 'اللافتة الافتراضية';
+$lang['Crop banner image'] = 'اقتصاص صورة اللافتة';
+$lang['Choose the part of the image you want to use as your header.'] = 'اختيار جزء من الصورة التي تريد استخدامها في ترويسة موقعك.';
+$lang['Banner deleted'] = 'حُذِفَت اللافتة';
+$lang['Choose an image from your computer'] = 'اختيار صورة من جهاز الكمبيوتر';
+$lang['Banner added'] = 'اضيفت اللافتة';
+$lang['Banner'] = 'لافتة';
+$lang['Album specific banners'] = 'لافتات الالبوم الخاصة';
+$lang['Add a banner'] = 'إضافة لافتة';
+?>
