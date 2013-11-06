020fd31f38968a62f816af28f8addb0b9e57c024
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-06 19:56:11 +0000

    [header_manager] Update eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@25362 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,41 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Album specific banners'] = 'Fotarospecifaj rubandoj';
+$lang['Choose an image from your computer'] = 'Elekti bildon en via komputilo';
+$lang['Choose the part of the image you want to use as your header.'] = 'Elekti la parton de la bildon, kiun vi deziras uzi kiel paĝokapo';
+$lang['Crop banner image'] = 'Stuci la rubandon';
+$lang['Default banner size'] = 'Apriora rubandograndeco';
+$lang['Display banner on photo page'] = 'Vidigi rubandon sur la fotopaĝo';
+$lang['Random'] = 'Hazarda';
+$lang['Select an image'] = 'Selekti bildon';
+$lang['Unknown picture id'] = 'Nekonata bildidenteco';
+$lang['With text'] = 'Kun teksto';
+$lang['Add a banner'] = 'Aldoni paĝan rubandon';
+$lang['Banner'] = 'Paĝa rubando';
+$lang['Banner added'] = 'Paĝa rubando aldoniĝis';
+$lang['Banner deleted'] = 'Paĝa rubando foriĝis';
+$lang['Default banner'] = 'Apriora paĝa rubando';
+$lang['Image only'] = 'Nur bildo';
+$lang['Incorrect file type,'] = 'Malĝusta dosiertipo,';
+$lang['No banner added yet'] = 'Ankoraŭ neniu paĝa rubando aldoniĝis';
+?>
