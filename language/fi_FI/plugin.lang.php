be7041e866ab3041671631123a3e516a8b92d601
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-17 21:54:43 +0000

    [header_manager] Update fi_FI, thanks to : lrd
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@25989 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Random'] = 'Satunnainen';
+$lang['Recursive'] = 'Rekursiivinen';
+$lang['Restore default banner'] = 'Palauta oletus banneri';
+$lang['Select an image'] = 'Valitse kuva';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Kuvan numero tunniste löytyy kuvan muokkaussivulta, läheltä esikatselu kuvaa';
+$lang['Unknown picture id'] = 'Tuntematon kuvan tunniste';
+$lang['Use'] = 'Käytä';
+$lang['With text'] = 'Tekstin kanssa';
+$lang['You can customize the display by adding CSS rules to'] = 'Voit muokata näkymää lisäämällä CSS sääntöjä';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Voit lisätä kustomoidun ylätunniste kuvan tai valita kuvan galleriastasi. Seuraavassa vaiheessa voit leikata kuvan sopivaksi.';
+$lang['or choose a picture from the gallery'] = 'tai valitse kuva galleriasta';
+$lang['Add a banner'] = 'Lisää banneri';
+$lang['Album specific banners'] = 'Albumi kohtaiset bannerit';
+$lang['Banner'] = 'Banneri';
+$lang['Banner added'] = 'Banneri lisätty';
+$lang['Banner deleted'] = 'Banneri poistettu';
+$lang['Choose an image from your computer'] = 'Valitse kuva tietokoneeltasi';
+$lang['Choose the part of the image you want to use as your header.'] = 'Valitse osa kuvasta käytettäväksi ylätunnisteenasi';
+$lang['Crop banner image'] = 'Leikkaa banneri kuvaa';
+$lang['Default banner'] = 'Oletus banneri';
+$lang['Default banner size'] = 'Oletus bannerin koko';
+$lang['Display banner on photo page'] = 'Näytä banneri kuva sivulla';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'MontBlancXL ja BlancMontXL:ää varteen, suositeltu kuvakoko on 900&times;190 pikseliä.';
+$lang['Gallery title above image'] = 'Gallerian otsikko kuvan yläpuolella';
+$lang['Image only'] = 'Vain kuva';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'Lisätäksesi tietyn bannerin, mene halutun albumin admin-sivulle.';
+$lang['Incorrect file type,'] = 'Väärä tiedostotyyppi,';
+$lang['No banner added yet'] = 'Banneria ei ole vielä lisätty';
+$lang['Non recursive'] = 'Ei rekursiivinen';
+$lang['Picture id.'] = 'Kuvan tunniste.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Lisää <b>%header_manager%</b> paikkaan, jossa haluat näyttää kuvan.';
+?>
