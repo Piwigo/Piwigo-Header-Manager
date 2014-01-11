269fce916e58344a8ec0a113726679b1e83cc67e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-11 14:22:10 +0000

    [header_manager] Update fa_IR, thanks to : ipic
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@26621 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,58 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = 'افزودن یک بنر ';
+$lang['Random'] = 'اتفاقی';
+$lang['Recursive'] = 'بازگشت';
+$lang['Respect %s aspect ratio'] = ' %sنسبت تصویر رابطه از   ';
+$lang['Restore default banner'] = '
+تنظیمات پیش فرض بنر';
+$lang['Select an image'] = 'اتخاب تصویر';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'شناسه عددی را می توان در صفحه ویرایش تصویر، در نزدیکی تصویر بند انگشتی یافت.';
+$lang['Unknown picture id'] = 'شاناسه عکس نامشخص است';
+$lang['Use'] = 'استفاده';
+$lang['With text'] = 'با متن';
+$lang['You can customize the display by adding CSS rules to'] = 'شما می توانید نمایش را با اضافه کردن قوانین CSS برای سفارشی';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = '
+شما می توانید یک تصویر سربرگ و یا یکی از گالری خود انتخاب کنید. در صفحه بعد شما قادر خواهید بود برای برش تصویر خواهد بود.';
+$lang['or choose a picture from the gallery'] = '
+یا انتخاب کنید یک عکس از گالری ';
+$lang['Banner'] = 'بنر';
+$lang['Banner added'] = 'افزودن بنر';
+$lang['Banner deleted'] = 'بنرپاک شد  ';
+$lang['Banner size'] = 'اندازه بنر';
+$lang['Choose an image from your computer'] = 'انتخاب تصویر از روی رایانه ';
+$lang['Choose the part of the image you want to use as your header.'] = 'بخشی از تصویر را که می خواهید به عنوان هدر های خود استفاده کنید را انتخاب کنید.';
+$lang['Crop banner image'] = 'برید بنر ';
+$lang['Default banner'] = 'بنر پیشفرض';
+$lang['Display banner on photo page'] = 'نمایش بنر در صفحه تصویر ';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'برای ادازه و زمان مناسب MontBlancXL و BlancMontXL، 
+900&times;190.';
+$lang['Gallery title above image'] = 'عنوان گالری تصویر بالا';
+$lang['Image only'] = 'فقط عکس';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'به منظور اضافه کردن یک بنر خاص، به صفحه مدیر از آلبوم دلخواه بروید.';
+$lang['Incorrect file type,'] = 'نوع فایل نادرست،';
+$lang['No banner added yet'] = 'هیچ بنری افزوده شد :(';
+$lang['Non recursive'] = '
+بدون بازگشت';
+$lang['Picture id.'] = 'شناسه تصویر ';
+?>
