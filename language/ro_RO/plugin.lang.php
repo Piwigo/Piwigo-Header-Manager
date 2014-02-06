096faaab854b3400c80edad5689002e312defdbf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-02-06 12:29:18 +0000

    [header_manager] Add ro_RO, thanks to : cheilenereinfo
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@27237 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,51 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Pentru MontBlancXL si BlancMontXL, dimensiunea potrivita este 900&times;190';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Puteti incarca o imagine in antet sau sa selectati o imagine din galerie. In fereastra urmatoare puteti sa decupati din imagine';
+$lang['You can customize the display by adding CSS rules to'] = 'Puteti personaliza afisarea prin adaugarea de reguli CSS la';
+$lang['Unknown picture id'] = 'Id-ul fotografiei necunoscut';
+$lang['Picture id.'] = 'Id-ul fotografiei';
+$lang['or choose a picture from the gallery'] = 'sau alege o imagine din galerie';
+$lang['Respect %s aspect ratio'] = 'Respecta %s raport de aspect';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Pune <b>%header_manager%</b> acolo unde vreti sa fie afisata imaginea';
+$lang['Gallery title above image'] = 'Titlul galeriei deasupra imaginii';
+$lang['With text'] = 'Cu text';
+$lang['Use'] = 'Foloseste';
+$lang['Select an image'] = 'Selecteaza o imagine';
+$lang['Restore default banner'] = 'Restabileste banner-ul implicit';
+$lang['Random'] = 'Aleator';
+$lang['No banner added yet'] = 'Nici un banner nu a fost adaugat';
+$lang['Incorrect file type,'] = 'Tip de fisier incorect';
+$lang['Display banner on photo page'] = 'Afiseaza banner in pagina cu fotografii';
+$lang['Default banner'] = 'Banner implicit';
+$lang['Crop banner image'] = 'Decupeaza imaginea din banner';
+$lang['Banner size'] = 'Dimensiunea banner-ului';
+$lang['Banner deleted'] = 'Banner sters';
+$lang['Banner added'] = 'Banner adaugat';
+$lang['Banner'] = 'Banner';
+$lang['Album specific banners'] = 'Bannere specifice unui album';
+$lang['Add a banner'] = 'Adauga un banner';
+$lang['Image only'] = 'Doar poza';
+$lang['Choose the part of the image you want to use as your header.'] = 'Alege o parte din imagine pe care o doresti ca antet ';
+$lang['Choose an image from your computer'] = 'Alege o imagine din calculator';
+?>
