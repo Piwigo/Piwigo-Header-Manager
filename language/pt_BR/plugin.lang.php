7ab8e7507b212ecd86079f71cf824620a5e200e3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-03-01 02:23:41 +0000

    [header_manager] Update pt_BR, thanks to : msakik
    git-svn-id: http://piwigo.org/svn/extensions/header_manager@27537 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pt_BR/plugin.lang.php
+++ language/pt_BR/plugin.lang.php
@@ -21,23 +21,23 @@
 // | USA.                                                                  |
 // +-----------------------------------------------------------------------+
 $lang['Picture id.'] = 'ID da foto';
-$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Ponha <b>%header_manager%</b> onde você quer mostrar a imagem.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Coloque <b>%header_manager%</b> onde você quiser mostrar a imagem.';
 $lang['Random'] = 'Aleatório';
 $lang['Select an image'] = 'Selecione uma imagem';
-$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'O identificador numérico pode ser encontrado na página de edição da foto, próximo a miniatura da mesma.';
-$lang['Unknown picture id'] = 'Id da foto desconhecido.';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'O identificador numérico pode ser encontrado na página de edição da foto, próximo à miniatura da mesma.';
+$lang['Unknown picture id'] = 'Id da foto desconhecido';
 $lang['Use'] = 'Use';
 $lang['With text'] = 'Com texto';
 $lang['You can customize the display by adding CSS rules to'] = 'Você pode personalizar a exibição adicionado regras de CSS';
-$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Você pode enviar uma imagem personalizada do cabeçalho ou selecionar uma de seus albuns. Na próxima tela você poderá recortar a imagem.';
-$lang['or choose a picture from the gallery'] = 'ou escolha uma foto dos seus álbuns';
-$lang['Display banner on photo page'] = 'Mostra banner na página de foto';
-$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Para MontBlanXL e BlanMontXL, aconselha-se o tamanho 900&times;190.';
-$lang['Gallery title above image'] = 'Título do Álbum abaixo da Imagem';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Você pode enviar uma imagem de cabeçalho personalizada ou selecionar uma de sua galeria. Na próxima tela você poderá recortar a imagem.';
+$lang['or choose a picture from the gallery'] = 'ou escolha uma foto de sua galeria';
+$lang['Display banner on photo page'] = 'Mostre banner na página de foto';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Para MontBlanXL e BlanMontXL, aconselha-se o tamanho de 900&times;190.';
+$lang['Gallery title above image'] = 'Título da galeria por cima da imagem';
 $lang['Image only'] = 'Imagem somente';
 $lang['Incorrect file type,'] = 'Tipo de arquivo incorreto';
 $lang['No banner added yet'] = 'Nenhum banner adicionado ainda';
-$lang['Add a banner'] = 'Adicione um Banner';
+$lang['Add a banner'] = 'Adicione um banner';
 $lang['Banner'] = 'Banner';
 $lang['Banner added'] = 'Banner adicionado';
 $lang['Banner deleted'] = 'Banner apagado';
@@ -46,10 +46,10 @@ $lang['Choose the part of the image you want to use as your header.'] = 'Escolha
 $lang['Crop banner image'] = 'Recorta a imagem do Banner';
 $lang['Default banner'] = 'Banner padrão';
 $lang['Album specific banners'] = 'Banners para álbuns específicos';
-$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'Para poder adicionar um banner específico, vá para a página administrativa do álbum desejado.';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'Para adicionar um banner específico, vá para a página administrativa do álbum desejado.';
 $lang['Non recursive'] = 'Não recursivo';
 $lang['Recursive'] = 'Recursivo';
-$lang['Restore default banner'] = 'Restaurar banner padrão.';
+$lang['Restore default banner'] = 'Restaurar banner padrão';
 $lang['Banner size'] = 'Tamanho do banner';
 $lang['Respect %s aspect ratio'] = 'Respeitar a razão da proporção %s';
 ?>
