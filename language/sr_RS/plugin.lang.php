983fc5cb71ea3da8ba5a29e12752aa000d026edc
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-12-24 13:31:49 +0100

    Update sr_RS, thanks to knjigor

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,30 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = 'Додај банер';
+$lang['Album specific banners'] = 'Банери специфични за албум';
+$lang['Banner'] = 'Банер';
+$lang['Banner added'] = 'Банер додат';
+$lang['Banner deleted'] = 'Банер обрисан';
+$lang['Banner size'] = 'Величина банера';
+$lang['Choose an image from your computer'] = 'Изаберите слику са рачунара';
+$lang['Choose the part of the image you want to use as your header.'] = 'Изаберите део слике који желите да искористите за хедер';
