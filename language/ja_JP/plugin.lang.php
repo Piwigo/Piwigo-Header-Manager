d7fb5fd9a9ea75036601705324dd2d092179314c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2016-03-21 19:50:15 +0100

    Add ja_JP, thanks to ryoon

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2015 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Random'] = 'ランダム';
+$lang['Recursive'] = '再帰的';
+$lang['Respect %s aspect ratio'] = '%sのアスペクト比を保持する';
+$lang['Restore default banner'] = 'デフォルトのバナーを復元する';
+$lang['Select an image'] = 'イメージを選択してください';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = '識別番号は、写真編集ページのサムネイルの近くにあります';
+$lang['Unknown picture id'] = '未知の写真ID';
+$lang['Use'] = '使う';
+$lang['With text'] = '付属テキスト';
+$lang['You can customize the display by adding CSS rules to'] = 'CSSルールを追加することで、表示をカスタマイズすることができます';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = '独自のヘッダーイメージをアップロードするか、ギャラリーから選択することができます。次の画面で、イメージの部分を指定できます';
+$lang['or choose a picture from the gallery'] = 'あるいは、ギャラリーから写真を選択します';
+$lang['Add a banner'] = 'バナーを追加する';
+$lang['Album specific banners'] = 'アルバム独自のバナー';
+$lang['Banner'] = 'バナー';
+$lang['Banner added'] = 'バナーが追加されました';
+$lang['Banner deleted'] = 'バナーが削除されました';
+$lang['Banner size'] = 'バナーのサイズ';
+$lang['Choose an image from your computer'] = 'あなたのコンピューターからイメージを選択する';
+$lang['Choose the part of the image you want to use as your header.'] = 'イメージからヘッダーで使いたい部分を選択する';
+$lang['Crop banner image'] = 'バナーイメージの部分を選択する';
+$lang['Default banner'] = 'デフォルトのバナー';
+$lang['Display banner on photo page'] = '写真のページでバナーを表示する';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'MontBlancXLとBlancMontXLには、900&times;190のサイズが推奨されています';
+$lang['Gallery title above image'] = 'イメージの丈夫のギャラリータイトル';
+$lang['Image only'] = 'イメージのみ';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = '特定のバナーを追加するには、管理画面でアルバムごとに設定します';
+$lang['Incorrect file type,'] = 'ファイル形式が正しくありません';
+$lang['No banner added yet'] = 'バナーがまだ追加されていません';
+$lang['Non recursive'] = '非再帰的';
+$lang['Picture id.'] = '写真ID';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = '<b>%header_manager%</b>をイメージを表示させたい場所に挿入してください';
