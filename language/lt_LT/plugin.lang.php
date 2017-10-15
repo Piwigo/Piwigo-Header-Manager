f55f678ea971ff740d160fa8c9610e4ae19797e7
Piwigo-TranslationTeam <translate-github@piwigo.org>
2017-10-15 07:05:46 +0200

    Add lt_LT, thanks to vilisas

---
+++ language/lt_LT/plugin.lang.php
@@ -0,0 +1,54 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2017 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Random'] = 'Atsitiktinė';
+$lang['Recursive'] = 'Rekursyviai';
+$lang['Respect %s aspect ratio'] = 'Laikytis %s vaizdo santykio';
+$lang['Restore default banner'] = 'Atstatyti pradinę reklamjuostę';
+$lang['Select an image'] = 'Pasirinkite paveikslėlį';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Skaitinį identifikatorių galite rasti paveikslėlio redagavimo puslapyje, šalia miniatiūros.';
+$lang['Unknown picture id'] = 'Nežinomas paveikslėlio ID';
+$lang['Use'] = 'Naudoti';
+$lang['With text'] = 'Su tekstu';
+$lang['You can customize the display by adding CSS rules to'] = 'Galite pritaikyti išvaizdą pridėdami CSS taisykles prie';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Galite įkelti atskirą antraštės paveikslėlį arba jį pasirinkti iš galerijos. Kitame puslapyje galėsite jį apkarpyti.';
+$lang['or choose a picture from the gallery'] = 'arba pasirinkite paveikslėlį iš galerijos';
+$lang['Add a banner'] = 'Pridėti reklamjuostę';
+$lang['Album specific banners'] = 'Specifinės albumų reklamjuostės';
+$lang['Banner'] = 'Reklamjuostė';
+$lang['Banner added'] = 'Reklamjuostė pridėta';
+$lang['Banner deleted'] = 'Reklamjuostė pašalinta';
+$lang['Banner size'] = 'Reklamjuostės dydis';
+$lang['Choose an image from your computer'] = 'Pasirinkite paveiksliuką iš savo kompiuterio';
+$lang['Choose the part of the image you want to use as your header.'] = 'Pasirinkite paveikslėlio dalį, kurią Jūs norite naudoti kaip antraštę.';
+$lang['Crop banner image'] = 'Apkarpyti reklamjuostės paveiksliuką';
+$lang['Default banner'] = 'Reklamjuostė pagal nutylėjimą';
+$lang['Display banner on photo page'] = 'Rodyti reklamjuostę nuotraukų puslapyje';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'MontBlancXL ir BlancMontXl rekomenduojamas dydis yra 900&times;190.';
+$lang['Gallery title above image'] = 'Galerijos pavadinimas virš paveikslėlio';
+$lang['Image only'] = 'Tik paveikslėlio';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'Tam, kad pridėtumėte specifinę reklamjuostę, eikite į to albumo valdymo puslapį.';
+$lang['Incorrect file type,'] = 'Neteisingas failo tipas.';
+$lang['No banner added yet'] = 'Dar nepridėta jokių reklamjuosčių';
+$lang['Non recursive'] = 'Ne rekursyviai';
+$lang['Picture id.'] = 'Paveikslėlio ID.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Įdėkite <b>%header_manager%</b> ten, kur norite rodyti paveikslėlį.';
