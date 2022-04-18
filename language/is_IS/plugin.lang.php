bde10a829907971bfd66112bf6c52aa96a56507b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-18 23:01:44 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['or choose a picture from the gallery'] = 'eða veldu mynd úr myndasafninu';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'Fyrir MontBlancXL og BlancMontXL, ráðlögð stærð er 900&times;190.';
+$lang['Gallery title above image'] = 'Gallerí titill fyrir ofan mynd';
+$lang['Image only'] = 'Eingöngu mynd';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'Til að bæta við ákveðnum borða skaltu fara á stjórnunarsíðu viðkomandi albúms.';
+$lang['Incorrect file type,'] = 'Röng skráargerð,';
+$lang['No banner added yet'] = 'Engum borða bætt við ennþá';
+$lang['Non recursive'] = 'Ekki endurkvæmt';
+$lang['Picture id.'] = 'Myndauðkenni';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'Settu <b>%header_manager%</b> þar sem þú vilt birta myndina.';
+$lang['Random'] = 'Handahófi';
+$lang['Recursive'] = 'Endurkvæmt';
+$lang['Respect %s aspect ratio'] = 'Virða %s stærðarhlutföll';
+$lang['Restore default banner'] = 'Endurheimta sjálfgefna borða';
+$lang['Select an image'] = 'Velja mynd';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'Töluauðkennið er að finna á myndútgáfusíðunni, nálægt smámyndinni.';
+$lang['Unknown picture id'] = 'Óþekkt myndauðkenni';
+$lang['Use'] = 'Nota';
+$lang['With text'] = 'Með texta';
+$lang['You can customize the display by adding CSS rules to'] = 'Þú getur sérsniðið skjáinn með því að bæta CSS reglum við';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'Þú getur hlaðið upp sérsniðinni hausmynd eða valið eina úr myndasafninu þínu. Á næsta skjá geturðu klippt myndina.';
+$lang['Add a banner'] = 'Bættu við borða';
+$lang['Album specific banners'] = 'Sérstakir borðar albúms';
+$lang['Banner'] = 'Borði';
+$lang['Banner added'] = 'Borða bætt við';
+$lang['Banner deleted'] = 'Borða eytt';
+$lang['Banner size'] = 'Borðastærð';
+$lang['Choose an image from your computer'] = 'Veldu mynd frá tölvunni þinni';
+$lang['Choose the part of the image you want to use as your header.'] = 'Veldu þann hluta myndarinnar sem þú vilt nota sem haus.';
+$lang['Crop banner image'] = 'Kroppa borðamynd';
+$lang['Default banner'] = 'Sjálfgefinn borði';
+$lang['Display banner on photo page'] = 'Birta borða á myndasíðu';
