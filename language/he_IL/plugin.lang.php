41ec91b7f701f60c3f37f3c41005cd0f91aac7da
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 16:16:18 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Add a banner'] = 'הוסף באנר';
+$lang['Album specific banners'] = 'באנרים ספציפיים לאלבום';
+$lang['Banner'] = 'באנר';
+$lang['Banner added'] = 'הבאנר נוסף';
+$lang['Banner deleted'] = 'הבאנר נמחק';
+$lang['Banner size'] = 'גודל הבאנר';
+$lang['Choose an image from your computer'] = 'בחר תמונה מהמחשב שלך';
+$lang['Choose the part of the image you want to use as your header.'] = 'בחר את החלק של התמונה שברצונך להשתמש בו ככותרת.';
+$lang['Crop banner image'] = 'חתוך תמונת באנר';
+$lang['Default banner'] = 'באנר ברירת מחדל';
+$lang['Display banner on photo page'] = 'הצג באנר בדף התמונה';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'עבור MontBlancXL ו-BlancMontXL, הגודל המומלץ הוא 900&times;190.';
+$lang['Gallery title above image'] = 'כותרת גלריה מעל התמונה';
+$lang['Image only'] = 'תמונה בלבד';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = 'כדי להוסיף באנר ספציפי, עבור לדף המנהל של האלבום הרצוי.';
+$lang['Incorrect file type,'] = 'סוג קובץ שגוי,';
+$lang['No banner added yet'] = 'עדיין לא הוסף באנר';
+$lang['Non recursive'] = 'לא רקורסיבי';
+$lang['Picture id.'] = 'מזהה תמונה.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = 'שים <b>%header_manager%</b> במקום שבו ברצונך להציג את התמונה.';
+$lang['Random'] = 'אקראי';
+$lang['Recursive'] = 'רקורסיבי';
+$lang['Respect %s aspect ratio'] = 'שמור על יחס הרוחב לגובה של %s';
+$lang['Restore default banner'] = 'שחזר באנר ברירת מחדל';
+$lang['Select an image'] = 'בחר תמונה';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = 'המזהה המספרי ניתן למציאה בדף עריכת התמונה, ליד התמונה הקטנה.';
+$lang['Unknown picture id'] = 'מזהה תמונה לא ידוע';
+$lang['Use'] = 'השתמש';
+$lang['With text'] = 'עם טקסט';
+$lang['You can customize the display by adding CSS rules to'] = 'ניתן להתאים אישית את התצוגה על ידי הוספת כללי CSS ל';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = 'ניתן להעלות תמונת כותרת מותאמת אישית או לבחור אחת מהגלריה שלך. במסך הבא תוכל לחתוך את התמונה.';
+$lang['or choose a picture from the gallery'] = 'או לבחור תמונה מהגלריה';
