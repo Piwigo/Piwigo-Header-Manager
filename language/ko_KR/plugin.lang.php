9ae79feae91a95526ab0553b5a2321830c58d6b0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-03 06:57:16 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Random'] = '무작위';
+$lang['Recursive'] = '재귀적';
+$lang['Respect %s aspect ratio'] = '%s 화면비율 유지';
+$lang['Restore default banner'] = '기본 배너 복원';
+$lang['Select an image'] = '이미지 선택';
+$lang['The numeric identifier can be found on the picture edition page, near the thumbnail.'] = '숫자 식별자는 사진 편집 페이지의 썸네일 근처에서 찾을 수 있습니다.';
+$lang['Unknown picture id'] = '알 수 없는 사진 ID';
+$lang['Use'] = '사용';
+$lang['With text'] = '텍스트 포함';
+$lang['You can customize the display by adding CSS rules to'] = 'CSS 규칙을 추가하여 표시를 맞춤 설정할 수 있습니다.';
+$lang['You can upload a custom header image or select one from your gallery. On the next screen you will be able to crop the image.'] = '사용자 지정 헤더 이미지를 업로드하거나 갤러리에서 선택할 수 있습니다. 다음 화면에서 이미지를 자를 수 있습니다.';
+$lang['or choose a picture from the gallery'] = '또는 갤러리에서 사진을 선택하세요';
+$lang['Add a banner'] = '배너 추가';
+$lang['Album specific banners'] = '앨범별 배너';
+$lang['Banner'] = '배너';
+$lang['Banner added'] = '배너 추가됨';
+$lang['Banner deleted'] = '배너 삭제됨';
+$lang['Banner size'] = '배너 크기';
+$lang['Choose an image from your computer'] = '컴퓨터에서 이미지 선택';
+$lang['Choose the part of the image you want to use as your header.'] = '헤더로 사용할 이미지 부분 선택';
+$lang['Crop banner image'] = '배너 이미지 자르기';
+$lang['Default banner'] = '기본 배너';
+$lang['Display banner on photo page'] = '사진 페이지에 배너 표시';
+$lang['For MontBlancXL and BlancMontXL, advised size is 900&times;190.'] = 'MontBlancXL 및 BlancMontXL의 권장 크기는 900×190입니다.';
+$lang['Gallery title above image'] = '이미지 상단 갤러리 제목';
+$lang['Image only'] = '이미지만';
+$lang['In order to add a specific banner, go to the admin page of the desired album.'] = '특정 배너를 추가하려면 원하는 앨범의 관리자 페이지로 이동하세요.';
+$lang['Incorrect file type,'] = '잘못된 파일 형식,';
+$lang['No banner added yet'] = '아직 배너가 추가되지 않았습니다';
+$lang['Non recursive'] = '비재귀적';
+$lang['Picture id.'] = '사진 ID.';
+$lang['Put <b>%header_manager%</b> where you want to display the image.'] = '이미지를 표시할 위치에 <b>%header_manager%</b>를 입력하세요.';
